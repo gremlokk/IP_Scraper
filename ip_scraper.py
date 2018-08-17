f5e536d2ee6dd356b971f932e155f8b03e72e940
gremlokk <fredrickryans@yahoo.com>
2018-08-17 17:54:13 -0500

    Update read code

--- ip_scraper.py
+++ ip_scraper.py
@@ -2,7 +2,6 @@
 Fredrick Ryans
 8/16/2018
 Version: 1.0
-
 The purpose of this script is to find the number of IP addresses
 that may be causing a clients bandwidth to spike in Americommerce.
 '''
@@ -14,13 +13,13 @@ from collections import Counter
 try:
     file = str(input("Enter a file path: "))
     min_hits = int(input("Enter a number for the minimum amount of hits to track for an IP address: "))
-
+    col_name = 'IPaddress'
     print("Program Started.\n")
-    
+
     with open(file) as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=',')
-        list_of_ips = [row[9] for row in readCSV]
-    
+        readCSV = csv.DictReader(csvfile)
+        list_of_ips = [row[col_name] for row in readCSV]
+
     for key,value in Counter(list_of_ips).items():
         if int(value) > min_hits:
             print (key + " " + "hits: " + str(value))
